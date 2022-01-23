7d41a9cbce1460c6aad6356f3d8095b6623e1924
mdi <mihaidicianu@gmail.com>
2022-01-23 10:54:36 +0200

    Update config files

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,25 @@
 require("babel-register");
 require("babel-polyfill");
 
+const { projectId, mnemonic } = require("./secrets.json");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*", // Match any network id
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${projectId}`
+        ),
+      network_id: 4, // Rinkeby's id
+      gas: 4500000,
+      gasPrice: 10000000000,
     },
   },
   contracts_directory: "./src/contracts/",
